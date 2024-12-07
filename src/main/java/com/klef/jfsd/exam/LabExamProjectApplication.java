faabe94067b69f6fe8f92bf430c031e5ab5daf64
Keerthi P <151801163+kidzzooo@users.noreply.github.com>
2024-12-07 08:42:58 +0530

    intialize to openn

---
+++ src/main/java/com/klef/jfsd/exam/LabExamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.klef.jfsd.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabExamProjectApplication.class, args);
+	}
+
+}
